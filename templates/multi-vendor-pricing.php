8328237661e623903426645162fd7d52b579f276
Fahad Siddiqui <ssuet222@gmail.com>
2023-12-26 13:00:55 +0500

    Other all 16 pages done

---
+++ templates/multi-vendor-pricing.php
@@ -0,0 +1,154 @@
+<section class="plans-lpsec " id="pricing">
+    <div class="w-100 pt-155 pb-90 position-relative">
+        <div class="container-fluid">
+            <div class="main-head text-center">
+                <h2>Plans &amp; Pricing</h2>
+                <span>Affordable Price Packages</span>
+            </div>
+            <div class="row">
+                <div class="col-xl-3 col-md-6 col-sm-6">
+                    <div class="inner-price">
+                        <h4>Basic Plan</h4>
+                        <h3>$35 <sup>110</sup></h3>
+                        <p>A compressive solution for all your digital needs.</p> 
+                        <div class="main-btns">
+                            <a class="d-inline-block pkg_link open_package_model" href="javascript:void(0);"
+                                data-id="1" data-name="Basic" data-price="$35" data-service="Logo &amp; Identity"
+                                title="">Order
+                                Now</a>
+                            <a href="javascript:;" class="d-inline-block btn_black open_chat_btn">
+                                Live
+                                Chat</a>
+                        </div>
+                        <div class="content-mn">
+                            <h6>FEATURES</h6>
+                            <p>All the premium features are made affordable.</p> 
+
+                            <ul class="list-unstyled">
+                                <li><i class="fas fa-check"></i>4 Unique Logo Concepts</li>
+                                <li><i class="fas fa-check"></i>3 Revisions</li>
+                                <li><i class="fas fa-check"></i>Free Color Options</li>
+                                <li><i class="fas fa-check"></i>24-48 Hrs Turnaround Time</li>
+                                <li><i class="fas fa-check"></i>100% Custom Designs</li>
+                                <li><i class="fas fa-check"></i>Final Files (JPG)</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-xl-3 col-md-6 col-sm-6">
+                    <div class="inner-price">
+                        <h4>Standard
+                            Plan</h4>
+                        <h3>$75 <sup>250</sup></h3>
+                        <p>A compressive solution for all your digital needs.</p> 
+                    <div class="main-btns">
+                            <a class="d-inline-block pkg_link open_package_model" href="javascript:void(0);"
+                                data-id="2" data-name="Standard" data-price="$75"
+                                data-service="Logo &amp; Identity">Order Now</a>
+                            <a href="javascript:;" class="d-inline-block btn_black open_chat_btn">
+                                Live
+                                Chat</a>
+
+                    </div>
+                        <div class="content-mn">
+                            <h6>FEATURES</h6>
+                            <p>All the premium features are made affordable.</p> 
+
+                            <ul class="list-unstyled">
+                                <li><i class="fas fa-check"></i>10 Unique Logo Concepts</li>
+                                <li><i class="fas fa-check"></i>5 revisions</li>
+                                <li><i class="fas fa-check"></i>Business Card Design</li>
+                                <li><i class="fas fa-check"></i>Free Color Options</li>
+                                <li><i class="fas fa-check"></i>24-48 Hrs Turnaround Time</li>
+                              
+                                <li><i class="fas fa-check"></i>100% Custom Designs</li>
+                                <li><i class="fas fa-check"></i>Final Files (PNG, JPG)</li>
+
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-xl-3 col-md-6 col-sm-6">
+                    <div class="inner-price">
+                        <h4>Advance Plan</h4>
+                      <span class="bst-seller">Best Seller</span>
+                        <h3>$125 <sup>410</sup></h3>
+                        <p>A compressive solution for all your digital needs.</p> 
+                        <div class="main-btns">
+                            <a class="d-inline-block pkg_link open_package_model" href="javascript:void(0);"
+                                data-id="3" data-name="Advance" data-price="$125"
+                                data-service="Logo &amp; Identity">Order Now</a>
+                            <a href="javascript:;" class="d-inline-block btn_black open_chat_btn">
+                                Live
+                                Chat</a>
+
+                        </div>
+                        <div class="content-mn">
+                            <h6>FEATURES</h6>
+                            <p>All the premium features are made affordable.</p> 
+
+                            <ul class="list-unstyled">
+                                <li><i class="fas fa-check"></i>Unlimited Unique Logo Concepts</li>
+                                <li><i class="fas fa-check"></i>10 revisions</li>
+                                <li><i class="fas fa-check"></i>Stationery Design (Business Cards, Envelopes,
+                                    Letterheads)</li>
+                                <li><i class="fas fa-check"></i>Free Color Options</li>
+                                <li><i class="fas fa-check"></i>24-48 Hrs Turnaround Time</li>
+                          
+                                <li><i class="fas fa-check"></i>100% Custom Designs</li>
+                                <li><i class="fas fa-check"></i>Final Files (AI, PSD, EPS, PNG, JPG, PDF)</li>
+
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-xl-3 col-md-6 col-sm-6">
+                    <div class="inner-price">
+                        <h4>Premium Plan</h4>
+                        <h3>$175 <sup>460</sup></h3>
+                      <p>A compressive solution for all your digital needs.</p> 
+                        <div class="main-btns">
+                            <a class="d-inline-block pkg_link open_package_model" href="javascript:void(0);"
+                                data-id="100" data-name="Premium" data-price="$175"
+                                data-service="Logo &amp; Identity">Order Now</a>
+                            <a href="javascript:;" class="d-inline-block btn_black open_chat_btn">
+                                Live
+                                Chat</a>
+
+                        </div>
+                        <div class="content-mn">
+                            <h6>FEATURES</h6>
+                            <p>All the premium features are made affordable.</p> 
+
+                            <ul class="list-unstyled">
+                                <li><i class="fas fa-check"></i>Unlimited Unique Logo Concepts</li>
+                                <li><i class="fas fa-check"></i>Unlimited Revisions</li>
+                                <li><i class="fas fa-check"></i>Stationery Design (Business Cards, Envelopes,
+                                    Letterheads)</li>
+                                <li><i class="fas fa-check"></i>Social Media Design</li>
+                                <li><i class="fas fa-check"></i>Free Color Options</li>
+                                <li><i class="fas fa-check"></i>Icon Design</li>
+                                <li><i class="fas fa-check"></i>24-48 Hrs Turnaround Time</li>
+             
+                                <li><i class="fas fa-check"></i>100% Custom Designs</li>
+                                <li><i class="fas fa-check"></i>Final Files (AI, PSD, EPS, PNG, JPG, PDF)</li>
+
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="lbn-btns">
+                <a href="packages.html" class="thm-btn mini-btn brd-rd3">View More Packages</a>
+                <a href="javascript:;" class="thm-btn mini-btn brd-rd3" data-toggle="modal"
+                    data-target="#myModal">Get In Touch</a>
+
+            </div>
+        </div>
+    </div>
+</section>
